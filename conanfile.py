666c858a052231468dcaa6e3afd4586eef729932
Esteban Dugueperoux <43169544+EstebanDugueperoux2@users.noreply.github.com>
2023-02-06 11:26:43 +0000

    Reproduce cmake link error

--- conanfile.py
+++ conanfile.py
@@ -20,38 +20,27 @@ class Qt5ExampleConan(ConanFile):
     }
 
     default_options = {
-        "shared": True,
+        "shared": False,
         "fPIC": True,
-        # "qt:cross_compile": "/usr/bin/arm-linux-gnueabihf-",
-        # "qt:device": "linux-arm-generic-g++",
-        # "qt:config": "-device-option DISTRO_OPTS=hard-float -extprefix",
-        # "qt:sysroot": "/usr/arm-linux-gnueabihf/",
-        # "qt:qtx11extras": False,
-        
         "qt:shared": True,
-        "qt:opengl": "no",
-        "qt:with_harfbuzz": False,
-        "qt:with_libjpeg": False,
-        "qt:with_libpng": False,
+        "qt:gui": False,
+        "qt:widgets": False,
         "qt:with_sqlite3": False,
         "qt:with_mysql": False,
         "qt:with_pq": False,
         "qt:with_odbc": False,
-        "qt:with_openal": False,
-        "qt:with_md4c": True,
-        "qt:with_x11": False,
-        "qt:gui": False,
-        "qt:widgets": False,
     }
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "src/*", "include/*"
     
     def requirements(self):
+        self.requires('boost/1.69.0')
         self.requires('qt/5.15.8')
+        self.requires("zlib/1.2.12", override=True)
 
     def build_requirements(self):
-        self.tool_requires("cmake/3.25.0")
+        self.tool_requires("cmake/3.25.1")
         self.tool_requires("ninja/1.11.1")
 
     def layout(self):
